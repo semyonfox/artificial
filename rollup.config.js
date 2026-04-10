9086bb317be2734a189477943789703b9dd32dd5
Semyon Fox <semyon.fox@gmail.com>
2026-04-10 20:48:01 +0100

    add rollup terser plugin with sourcemaps for production builds

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,12 @@
+import terser from '@rollup/plugin-terser';
+
 export default {
 	input: './game.js',
 	output: {
 		file: './dist/bundle.js',
 		format: 'iife',
 		name: 'Game',
+		sourcemap: true,
 	},
+	plugins: [terser()],
 };
